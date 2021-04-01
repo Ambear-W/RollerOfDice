56ea5a9d2e10b8e0918305b6c5df097505b0f2ea
Amber W <43912805+Ambear-W@users.noreply.github.com>
2021-03-31 22:57:09 -0400

    got the dice to roll!

--- app/src/main/java/com/example/rollerofdice/MainActivity.java
+++ app/src/main/java/com/example/rollerofdice/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.rollerofdice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,8 +18,10 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     RadioGroup radioGroupMod;
-    RadioButton radioButtonMod;
+    RadioButton plus, minus;
     TextView finalResult;
+    TextView rollBreakDownText;
+    Vibrator vibrator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +29,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final Button regRoll = findViewById(R.id.regRoll);
+        vibrator = (Vibrator)getSystemService(VIBRATOR_SERVICE);
 
+        //if the roll button is click then roll the dice and vibrate the phone when done
         regRoll.setOnClickListener(new View.OnClickListener(){
-
             public void onClick(View v){
-                //after the bottom is clicked grab the information
+                //after the button is clicked grab the information
 
                 //gets the text from numberOfDiceInput
                 EditText editText =findViewById(R.id.numberOfDiceInput);
@@ -48,12 +52,13 @@ public class MainActivity extends AppCompatActivity {
 
                 //check for mod type seleciton (plus or minus)
                 radioGroupMod = findViewById(R.id.modTypeGroup);
-                int radioId = radioGroupMod.getCheckedRadioButtonId();
-                radioButtonMod = findViewById(radioId);
+                plus = findViewById(R.id.plus);
+                minus = findViewById(R.id.minus);
 
                 //gets text from modifierInput
                 editText =findViewById(R.id.modifierInput);
                 temp = editText.getText().toString();
+
                 int modifier = 0;
                 if (temp.equals("")){
                     modifier = 0;
@@ -61,25 +66,41 @@ public class MainActivity extends AppCompatActivity {
                     modifier = Integer.parseInt(temp);
                 }
 
+                //does the actual rolling
                 int rollTotal = 0;
+                String rollBreakDown = "";
+                int randNumber = 0;
                 Random rand = new Random();
                 for(int i = 1; i != numberOfDice+1; i++){
-                    int randNumber = rand.nextInt(diceType+1);
+                    randNumber = rand.nextInt(diceType) + 1;
 
-                    if(randNumber == 0){
-                        randNumber = 1;
+                    //adds text to the final roll to change on screen
+                    if(i == numberOfDice){
+                        rollBreakDown = rollBreakDown.concat(String.valueOf(randNumber) + " + (" + modifier + ")" );
+                    }else{
+                        rollBreakDown = rollBreakDown.concat(String.valueOf(randNumber) + " + (" + modifier + ") + ");
                     }
 
-                    if(radioButtonMod.equals("+")){
+                    //finds out the modifier and then add/minus it
+                    if(plus.isChecked()){
                         randNumber = randNumber + modifier;
-                    }else{
+                    }
+                    if(minus.isChecked()){
                         randNumber = randNumber - modifier;
                     }
+
                     rollTotal += randNumber;
                 }
+
+                //sets the text on screen to the roll
                 finalResult = findViewById(R.id.rollResult);
                 finalResult.setText(String.valueOf(rollTotal));
-                //Toast.makeText(MainActivity.this, radioButtonMod.getText(), Toast.LENGTH_LONG).show();
+
+                rollBreakDownText = findViewById(R.id.rollBreakDown);
+                rollBreakDownText.setText(String.valueOf(rollBreakDown));
+
+                //vibrates the phone
+                vibrator.vibrate(1000);
             }
         });
     }
