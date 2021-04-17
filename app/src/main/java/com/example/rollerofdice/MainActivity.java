d06869f6719293698b78f36bda9618de45238513
Amber W <43912805+Ambear-W@users.noreply.github.com>
2021-04-17 14:32:35 -0400

    Got it to save to the file and view on history
    Need to find a way to prevent the historyRoll.txt file from being overwritten each time there is a new roll

--- app/src/main/java/com/example/rollerofdice/MainActivity.java
+++ app/src/main/java/com/example/rollerofdice/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.rollerofdice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.view.View;
@@ -13,7 +14,18 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Random;
+
+import static java.sql.DriverManager.println;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,11 +34,16 @@ public class MainActivity extends AppCompatActivity {
     TextView finalResult;
     TextView rollBreakDownText;
     Vibrator vibrator;
+    TextView historyText;
 
     int numberOfDice = 0;
     int modifier = 0;
     int rollTotal = 0;
     int randNumber = 0;
+
+    String filename = "historyRoll.txt";
+    String filepath = "MyFileDir";
+    String fileContent = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,10 +53,55 @@ public class MainActivity extends AppCompatActivity {
         final Button regRoll = findViewById(R.id.regRoll);
         vibrator = (Vibrator)getSystemService(VIBRATOR_SERVICE);
 
+        final Button history = findViewById(R.id.history);
+        //File myExternalFile = new File(getExternalFilesDir(filepath), filename);
+
+        history.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                historyText = findViewById(R.id.historyRolls);
+
+                FileReader fr = null;
+                File myExternalFile = new File(getExternalFilesDir(filepath), filename);
+                StringBuilder stringBuilder = new StringBuilder();
+
+                if(finalResult == null || rollBreakDownText == null){
+
+                }else{
+                    finalResult.setText("");
+                    rollBreakDownText.setText("");
+                }
+
+                try{
+                    fr = new FileReader(myExternalFile);
+                    BufferedReader br = new BufferedReader(fr);
+                    String line = br.readLine();
+                    while(line != null){
+                        stringBuilder.append(line).append('\n');
+                        line = br.readLine();
+                    }
+
+                } catch(FileNotFoundException e){
+                    e.printStackTrace();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }finally{
+                    String fileContents = stringBuilder.toString();
+                    historyText.setText(fileContents);
+                }
+
+            }
+        });
+
         //if the roll button is click then roll the dice and vibrate the phone when done
         regRoll.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 //after the button is clicked grab the information
+                if(historyText == null){
+
+                }else{
+                    historyText.setText("");
+                }
 
                 //gets the text from numberOfDiceInput
                 EditText editText =findViewById(R.id.numberOfDiceInput);
@@ -102,6 +164,29 @@ public class MainActivity extends AppCompatActivity {
 
                 //vibrates the phone
                 vibrator.vibrate(1000);
+
+                //makes the rollHistory file and adds it into memory
+
+                fileContent = rollBreakDown + " = " + (String.valueOf(rollTotal)) + "\n";
+
+                if(!fileContent.equals("")){
+                    File myExternalFile = new File(getExternalFilesDir(filepath), filename);
+                    FileOutputStream fos = null;
+                    try{
+                        fos = new FileOutputStream(myExternalFile);
+                        fos.write(fileContent.getBytes());
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                 }
+
+                numberOfDice = 0;
+                modifier = 0;
+                rollTotal = 0;
+                randNumber = 0;
+
             }
         });
     }
